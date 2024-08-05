ed7853e409cad7bf771be7d03d55d38be4f5e220
FireIsGood <fireisadequate@gmail.com>
2024-08-04 20:23:25 -0700

    Change wording of blank tiles

--- src/rules.rs
+++ src/rules.rs
@@ -44,7 +44,7 @@ fn print_mine_rules(args: &Args) {
     );
     let expanded_rules = format!("\
         - {} and {} are mines and anti-mines, meaning you lose\n\
-        - Zero tiles :blue_square: have no adjacent mines\n\
+        - Blank tiles :blue_square: have no adjacent mines\n\
         - Medals :medal: are a numbered combination of mines equaling zero (:first_place: is 1 mine 1 anti, :second_place: is 2 mine 2 anti, etc. to 3, further are generic)\n\
         - Number tiles :one: are a positive combination of mines (2 mines and 1 anti-mine is :one:)\n\
         - Letter tiles :regional_indicator_a: are a negative combination of mines (1 mine and 2 anti-mines is :regional_indicator_a:)\
